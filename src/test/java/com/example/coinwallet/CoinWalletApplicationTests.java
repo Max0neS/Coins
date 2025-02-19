e70623a6c76244c91cca834c739d1e7c148359b4
Максим <cromon541161@gmail.com>
2025-02-20 00:15:49 +0300

    Sonar conection

---
+++ src/test/java/com/example/coinwallet/CoinWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coinwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinWalletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
